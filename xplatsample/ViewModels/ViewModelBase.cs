e769024203ba642c0578e0e6c80a8d7788fbbc90
Jan Mechtel <jmechtel@posteo.de>
2024-05-22 15:04:39 +0200

    initial

---
+++ xplatsample/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace xplatsample.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ xplatsample/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace xplatsample.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
