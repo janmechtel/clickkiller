f5fd5bf27a21fcc43b5d1a0f62c3a3601cda64ae
Jan Mechtel <jmechtel@posteo.de>
2024-06-24 22:09:20 +0200

    UI: ESC hides the window

--- clickkiller/Views/MainWindow.axaml.cs
+++ clickkiller/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace clickkiller.Views;
 
@@ -8,11 +9,20 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         this.Closing += MainWindow_Closing;
+        this.KeyDown += MainWindow_KeyDown;
     }
 
     private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
     {
         e.Cancel = true;
         this.Hide();
+    }
+
+    private void MainWindow_KeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Escape)
+        {
+            this.Hide();
+        }
     }
 }
