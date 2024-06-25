3d7f4e37b31705238e7721650e0ef8fcb784b185
Jan Mechtel <jmechtel@posteo.de>
2024-06-25 11:14:36 +0200

    UI: timestamp formatting

--- clickkiller/ViewModels/MainViewModel.cs
+++ clickkiller/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using System.Linq;
 using ReactiveUI;
 using clickkiller.Data;
 
@@ -11,7 +12,7 @@ namespace clickkiller.ViewModels
         private readonly DatabaseService _databaseService;
         private string _application;
         private string _notes;
-        private ObservableCollection<Issue> _issues;
+        private ObservableCollection<IssueViewModel> _issues;
 
         private bool _focusNotes;
 
@@ -20,7 +21,7 @@ namespace clickkiller.ViewModels
             _databaseService = new DatabaseService();
             SaveCommand = ReactiveCommand.Create(SaveIssue);
             FocusNotesCommand = ReactiveCommand.Create(() => FocusNotes = true);
-            DeleteIssueCommand = ReactiveCommand.Create<Issue>(DeleteIssue);
+            DeleteIssueCommand = ReactiveCommand.Create<IssueViewModel>(DeleteIssue);
             RefreshIssues();
         }
 
@@ -42,7 +43,7 @@ namespace clickkiller.ViewModels
             set => this.RaiseAndSetIfChanged(ref _notes, value);
         }
 
-        public ObservableCollection<Issue> Issues
+        public ObservableCollection<IssueViewModel> Issues
         {
             get => _issues;
             private set => this.RaiseAndSetIfChanged(ref _issues, value);
@@ -65,13 +66,42 @@ namespace clickkiller.ViewModels
 
         private void RefreshIssues()
         {
-            Issues = new ObservableCollection<Issue>(_databaseService.GetAllIssues());
+            var issues = _databaseService.GetAllIssues();
+            var issueViewModels = new ObservableCollection<IssueViewModel>();
+
+            DateTime? lastDate = null;
+            foreach (var issue in issues.OrderByDescending(i => i.Timestamp))
+            {
+                bool showDate = !lastDate.HasValue || issue.Timestamp.Date != lastDate.Value.Date;
+                issueViewModels.Add(new IssueViewModel(issue, showDate));
+                lastDate = issue.Timestamp;
+            }
+
+            Issues = new ObservableCollection<IssueViewModel>(issueViewModels);
         }
 
-        private void DeleteIssue(Issue issue)
+        private void DeleteIssue(IssueViewModel issueViewModel)
         {
-            _databaseService.DeleteIssue(issue.Id);
+            _databaseService.DeleteIssue(issueViewModel.Id);
             RefreshIssues();
+        }
+    }
+
+    public class IssueViewModel : ViewModelBase
+    {
+        public int Id { get; }
+        public DateTime Timestamp { get; }
+        public string Application { get; }
+        public string Notes { get; }
+        public bool ShowDate { get; }
+
+        public IssueViewModel(Issue issue, bool showDate)
+        {
+            Id = issue.Id;
+            Timestamp = issue.Timestamp;
+            Application = issue.Application;
+            Notes = issue.Notes;
+            ShowDate = showDate;
         }
     }
 }
