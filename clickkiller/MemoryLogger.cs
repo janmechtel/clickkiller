26438b0a49667b8d561c027e907b55543525caeb
Jan Mechtel <jmechtel@posteo.de>
2024-06-27 22:40:36 +0200

    surress warning in MemoryLogger

--- clickkiller/MemoryLogger.cs
+++ clickkiller/MemoryLogger.cs
@@ -6,15 +6,19 @@ namespace clickkiller;
 
 public class LogUpdatedEventArgs : EventArgs
 {
-    public string Text { get; set; }
+    public required string Text { get; set; }
 }
 
 public class MemoryLogger : ILogger
 {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     public event EventHandler<LogUpdatedEventArgs> LogUpdated;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     private readonly StringBuilder _sb = new StringBuilder();
 
-    public IDisposable BeginScope<TState>(TState state)
+#pragma warning disable CS8633 // Nullability in constraints for type parameter doesn't match the constraints for type parameter in implicitly implemented interface method'.
+    public IDisposable? BeginScope<TState>(TState state)
+#pragma warning restore CS8633 // Nullability in constraints for type parameter doesn't match the constraints for type parameter in implicitly implemented interface method'.
     {
         return null;
     }
@@ -24,7 +28,9 @@ public class MemoryLogger : ILogger
         return true;
     }
 
+#pragma warning disable CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+#pragma warning restore CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
     {
         lock (_sb) {
             var message = formatter(state, exception);
