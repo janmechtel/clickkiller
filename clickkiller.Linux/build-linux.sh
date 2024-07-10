1a8757920994ec7ef4bd3899ab2441da8e25711b
Jan Mechtel <jmechtel@posteo.de>
2024-07-10 13:23:00 +0200

    using google storage for distribution URL

--- clickkiller.Linux/build-linux.sh
+++ clickkiller.Linux/build-linux.sh
@@ -18,6 +18,18 @@ echo ""
 echo "Compiling with dotnet..."
 dotnet publish -c Release --self-contained -r linux-x64 -o "$PUBLISH_DIR"
 
+#echo ""
+#echo "Downloading Velopack Releases"
+#vpk download github --repoUrl https://github.com/janmechtel/clickkiller/ -o "$RELEASE_DIR"
+
 echo ""
 echo "Building Velopack Release v$BUILD_VERSION"
 vpk pack -u Clickkiller -v $BUILD_VERSION -o "$RELEASE_DIR" -p "$PUBLISH_DIR" --mainExe clickkiller.Linux
+
+echo ""
+echo "Uploading Velopack Releases"
+#vpk upload github --repoUrl https://github.com/janmechtel/clickkiller/ --publish --releaseName "ClickKiler $BUILD_VERSION" --tag v$BUILD_VERSION -o "$RELEASE_DIR" --token 
+gsutil -m cp -n $RELEASE_DIR/*.nupkg gs://clickkiller/ # -n skip existing files
+gsutil -m cp $RELEASE_DIR/*.json $RELEASE_DIR/RELEASES-* $RELEASE_DIR/*.AppImage $RELEASE_DIR/*.exe gs://clickkiller/
+
+
